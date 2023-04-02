384d6e41901f3f79d48e09cf7e0bd6a06d023861
m1guelten <footballmiguelten@gmail.com>
2023-04-02 21:28:02 +0300

    Виконана п'ята(1) збірка

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,21 @@
 from fastapi import APIRouter
+import numpy as mt
 
 router = APIRouter()
 
 
 @router.get('')
-def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+# def hello_world() -> dict:
+#     return {'msg': 'Hello, World!'}
+
+def matrix() -> dict:
+    matrix_a = mt.random.rand(10,10)
+    matrix_b = mt.random.rand(10,10)
+
+    product = mt.dot(matrix_a,matrix_b)
+    result = {
+        "matrix_a":matrix_a.tolist(),
+        "matrix_b":matrix_b.tolist(),
+        "product":product.tolist()
+    }
+    return result
